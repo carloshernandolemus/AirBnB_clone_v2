ec3979212ba47e1862726b1578fe30b84d611c1a
chmlsud <1188@holbertonschool.com>
2020-09-02 18:40:59 +0000

    Fix code

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,84 +1,76 @@
 #!/usr/bin/python3
-""" new class for sqlAlchemy """
-from os import getenv
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import (create_engine)
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import Base
-from models.state import State
+''' Class to stablish conection with the DB '''
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
+from sqlalchemy import MetaData
 from models.city import City
-from models.user import User
 from models.place import Place
+from models.state import State
+from models.user import User
 from models.review import Review
 from models.amenity import Amenity
+from models.base_model import BaseModel, Base
 
 
 class DBStorage:
-    """ create tables in environmental"""
+    ''' DBStorage class '''
+
     __engine = None
     __session = None
 
     def __init__(self):
-        user = getenv("HBNB_MYSQL_USER")
-        passwd = getenv("HBNB_MYSQL_PWD")
-        db = getenv("HBNB_MYSQL_DB")
-        host = getenv("HBNB_MYSQL_HOST")
-        env = getenv("HBNB_ENV")
+        ''' get environment variables '''
+        user = os.getenv('HBNB_MYSQL_USER')
+        pasw = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST')
+        db = os.getenv('HBNB_MYSQL_DB')
+        name = os.getenv('HBNB_ENV')
 
+        ''' setting engine '''
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(user, passwd, host, db),
+                                      .format(user, pasw, host, db),
                                       pool_pre_ping=True)
-
-        if env == "test":
-            Base.metadata.drop_all(self.__engine)
-
-    def all(self, cls=None):
-        """returns a dictionary
-        Return:
-            returns a dictionary of __object
-        """
-        dic = {}
-        if cls:
-            if type(cls) is str:
-                cls = eval(cls)
-            query = self.__session.query(cls)
-            for elem in query:
-                key = "{}.{}".format(type(elem).__name__, elem.id)
-                dic[key] = elem
-        else:
-            lista = [State, City, User, Place, Review, Amenity]
-            for clase in lista:
-                query = self.__session.query(clase)
-                for elem in query:
-                    key = "{}.{}".format(type(elem).__name__, elem.id)
-                    dic[key] = elem
-        return (dic)
+        if name == 'test':
+            meta = MetaData(self.__engine)
+            meta.drop_all()
 
     def new(self, obj):
-        """add a new element in the table
-        """
+        ''' create new row in database '''
         self.__session.add(obj)
 
     def save(self):
-        """save changes
-        """
+        ''' commit the changes into DB '''
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete an element in the table
-        """
-        if obj:
-            self.session.delete(obj)
+        ''' Delete object from DB '''
+        self.__session.delete(obj)
+
+    def all(self, cls=None):
+        ''' return dictionary with every objects '''
+        session = self.__session
+        classes = [User, State, City, Amenity, Place, Review]
+        dictionary = {}
+        if cls is None:
+            for cls in classes:
+                query = session.query(cls).all()
+                for instance in query:
+                    key = '{}.{}'.format(instance.__class__.__name__,
+                                         instance.id)
+                    dictionary[key] = instance
+        else:
+            query = session.query(eval(cls)).all()
+            for instance in query:
+                key = '{}.{}'.format(instance.__class__.__name__,
+                                     instance.id)
+                dictionary[key] = instance
+        return dictionary
 
     def reload(self):
-        """configuration
-        """
+        ''' reload info '''
         Base.metadata.create_all(self.__engine)
-        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(sec)
-        self.__session = Session()
-
-    def close(self):
-        """ calls remove()
-        """
-        self.__session.close()
+        session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        session = scoped_session(session)
+        self.__session = session
