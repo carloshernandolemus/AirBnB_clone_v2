ae947e3e2168d7eabd14b155978c8bdd5bfd5a26
chmlsud <1188@holbertonschool.com>
2020-09-03 00:58:52 +0000

    Fix the code

--- models/state.py
+++ models/state.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
 from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from os import environ
+from sqlalchemy import Integer, String, Column, DateTime, ForeignKey
+from sqlalchemy.orm import relationship, backref
+from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
-    if environ.get('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref="state", cascade="all, delete")
-
-    else:
-        @property
-        def cities(self):
-            """Gets cities related to state"""
-            # instance_list = []
-            # for key, obj in models.storage.all().items():
-            #     if obj.__class__.__name__ == 'City':
-            #         if obj.state_id == self.id:
-            #             instance_list.append(obj)
-            # return instance_list
-            return [city for key, city in models.storage.all(City).items()
-                    if city.state_id == self.id]
+    @property
+    def cities(self):
+        """ getter method for cities"""
+        from models import storage
+        cities_list = []
+        dictionary = storage.all(City)
+        if dictionary:
+            for k, v in dictionary.items():
+                if self.id == v.state_id:
+                    cities_list.append(v)
+        return cities_list
